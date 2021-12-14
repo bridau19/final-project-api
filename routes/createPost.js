97f1d600878090bd3de546f70ab2dbb3100514fb
bridau19 <bridau19@gmail.com>
2021-12-14 17:17:02 -0500

    edit addPost

--- routes/createPost.js
+++ routes/createPost.js
@@ -8,12 +8,13 @@ const db = firestore.getFirestore();
 // API Endpoint for submitting data through our form
 router.get("/", (req, res) => {
     const queryParams = req.query; // Query params from URL
-    const { imageAlt, imageSrc, postMessage, userId, username } = queryParams;
+    const { postTitle, imageAlt, imageSrc, postMessage, userId, username } = queryParams;
     
     // Submit post to Firebase
     const setPost = firestore.addDoc( 
         firestore.collection(db, "posts"), 
         {
+            postTitle,
             imageAlt, 
             imageSrc, 
             postMessage, 
